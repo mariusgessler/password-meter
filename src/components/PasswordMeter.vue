b7deccab5998943ca28d6b59a13e1f47fe134b47
mariusgessler <marius.gessler@gmail.com>
2020-05-21 23:22:24 +0200

    Add PasswordMeter component

---
+++ src/components/PasswordMeter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='password-meter__container'>
+    <Input />
+  </div>
+</template>
+
+<script>
+import Input from './Input.vue';
+
+export default {
+  components: {
+    Input,
+  },
+};
+
+</script>
+
+<style lang='scss'>
+.password-meter__container {
+  position: relative;
+  left: 50%;
+  top: 50%;
+  max-width: 300px;
+  transform: translate(-50%, -50%);
+  border-radius: 25px;
+  background: #EBECF0;
+  background-image: url("../assets/clean-textile.png");
+  box-shadow: 20px 20px 60px #c8c9cc,
+             -20px -20px 60px #ffffff;
+}
+</style>
